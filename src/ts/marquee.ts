7b4cfc920b9e44d25eb5cb85bcd6dacb3596772e
Pyx <git@notpyx.me>
2025-02-20 23:53:42 -0500

    fix marquee

--- src/ts/marquee.ts
+++ src/ts/marquee.ts
@@ -1,32 +1,30 @@
-// Copyright (c) 2025 iiPython
-// Marquee88 - Infinite marquee system for 88x31 buttons
 document.addEventListener("DOMContentLoaded", () => {
-    setTimeout(() => {
-        const gap = 20;
-        const amount = 1;
-        const element = document.getElementById("f88x31") as HTMLElement;
+    const gap = 0;
+    const amount = 1;
+    const element = document.getElementById("f88x31") as HTMLElement;
 
-        const box = element.querySelector("div") as HTMLElement;
+    const box = element.querySelector("div") as HTMLElement;
+    box.style.position = "relative";
 
-        box.style.position = "relative";
-
-        let left = 0, last = 0;
-        function frame(time) {
-            if (time - last >= 10) {
-                const truth = element.getBoundingClientRect().left;
-                const rect = box.children[0].getBoundingClientRect();
-
-                if ((rect.left + rect.width) <= truth) {
-                    box.appendChild(box.children[0]);
-                    left += rect.width + gap;
-                }
-
-                box.style.left = `${left}px`;
-                left -= amount;
-                last = time;
+    let left = 0, last = 0;
+    function frame(time) {
+        if (time - last >= 10) {
+            // Calculate bounding boxes
+            const truth = element.getBoundingClientRect().left;
+            const rect = box.children[0].getBoundingClientRect();
+        
+            // Push left button to right side
+            if ((rect.left + rect.width) <= truth) {
+                box.appendChild(box.children[0]);
+                left += rect.width + gap;
             }
-            requestAnimationFrame(frame);
+        
+            // Update box positioning
+            box.style.left = `${left}px`;
+            left -= amount;
+            last = time;
         }
         requestAnimationFrame(frame);
-    }, 100)
+    }
+    requestAnimationFrame(frame);
 });
