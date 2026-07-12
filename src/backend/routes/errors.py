002626ff7f7c947f188103fc0378cae91a4acacf
Pyx <git@notpyx.me>
2026-07-12 02:24:11 -0400

    fix

--- src/backend/routes/errors.py
+++ src/backend/routes/errors.py
@@ -9,4 +9,4 @@ router = APIRouter(prefix="/e") # ugh
 
 @app.exception_handler(404)
 def not_found(req: Request, error_code): # fastapi requires the 2nd param.. idk why..
-    return render(req, "404.html", status=404)
+    return render(req, "404", status=404)
